32c4b42b44479c0972b94a085499af64644710d0
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-03-25 17:42:17 -0300

    feat: working with global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/theme/default';
 import { Button } from './components/Button';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Button />
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  body {
+    background-color: #333;
+    color: #fff;
+  }
+`;
