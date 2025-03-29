f0d1a204a130f96e527aa2f2721175c50bf4ea29
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-03-28 23:56:57 -0300

    feat: Adding fonts and Default Layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { History } from './pages/History'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
   return (
     <Routes>
-      <Route Component={Home} path="/" />
-      <Route Component={History} path="/history" />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
