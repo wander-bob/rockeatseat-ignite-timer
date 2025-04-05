9b8e02df82d3fb9e9f2bafb466fc904a55454b27
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-04-05 10:45:38 -0300

    fix: Adding a reset logic to the counter; feat: Adding current timer to page title

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -62,11 +62,17 @@ export function Home() {
   }
 
   useEffect(() => {
+    let interval: number;
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate));
       }, 1000);
     }
+
+    return () => {
+      clearInterval(interval);
+    };
   }, [activeCycle]);
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
@@ -77,6 +83,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0');
   const seconds = String(secondsAmount).padStart(2, '0');
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `React Timer: ${minutes}:${seconds}`;
+    }
+  }, [minutes, seconds, activeCycle]);
 
   const task = watch('task');
   const isSubmitDisabled = !task;
